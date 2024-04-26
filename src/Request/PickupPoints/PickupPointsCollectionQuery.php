975f8b8dc909d253545be0d20881eb703be1824a
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-26 13:10:03 +0200

    Add assertions for the input

--- src/Request/PickupPoints/PickupPointsCollectionQuery.php
+++ src/Request/PickupPoints/PickupPointsCollectionQuery.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Setono\Shipmondo\Request\PickupPoints;
 
 use Setono\Shipmondo\Request\Query\Query;
+use Webmozart\Assert\Assert;
 
 final class PickupPointsCollectionQuery extends Query
 {
@@ -15,6 +16,12 @@ final class PickupPointsCollectionQuery extends Query
         string $address = null,
         string $city = null,
     ) {
+        Assert::stringNotEmpty($carrierCode, 'The carrier code must be a non-empty string');
+        Assert::stringNotEmpty($countryCode, 'The country code must be a non-empty string');
+        Assert::stringNotEmpty($zipCode, 'The zip code must be a non-empty string');
+        Assert::nullOrStringNotEmpty($address, 'The address must be a non-empty string');
+        Assert::nullOrStringNotEmpty($city, 'The city must be a non-empty string');
+
         parent::__construct(array_filter([
             'carrier_code' => $carrierCode,
             'country_code' => $countryCode,
